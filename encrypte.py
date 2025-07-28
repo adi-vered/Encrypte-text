6b8463937b9b3bc03a52b1587dc73e278826834c
Adi Vered <veredfamily36@gmail.com>
2025-07-28 19:26:55 +0300

    with ascii values

--- encrypte.py
+++ encrypte.py
@@ -1,5 +1,11 @@
 def encrypte_text(secret, text):
     result = ""
-    for chr in text:
-        result += chr
+    for i in range(len(text)):
+        text_char = text[i]
+        secret_char = secret[i % len(secret)]
+        char_to_add = chr(ord(text_char) + ord(secret_char))
+        print(ord(char_to_add))
+        result += char_to_add
     return result
+
+print(encrypte_text("123456", "hello"))
