17099017420c0253cce77be4d48bf558f9c3ddaa
Adi Vered <veredfamily36@gmail.com>
2025-07-28 19:34:19 +0300

    with list

--- encrypte.py
+++ encrypte.py
@@ -1,11 +1,10 @@
 def encrypte_text(secret, text):
-    result = ""
+    result = []
     for i in range(len(text)):
         text_char = text[i]
         secret_char = secret[i % len(secret)]
-        char_to_add = chr(ord(text_char) + ord(secret_char))
-        print(ord(char_to_add))
-        result += char_to_add
+        char_to_add = ord(text_char) + ord(secret_char)
+        result += [char_to_add]
     return result
 
 print(encrypte_text("123456", "hello"))
