89c8db3bac46276ecde30c6e093ff962c83f969c
Adi Vered <veredfamily36@gmail.com>
2025-07-30 18:47:33 +0300

    fix with teacher

--- encrypte.py
+++ encrypte.py
@@ -1,13 +1,10 @@
 secret = input("Enter your key: ")
-text = input("Enter your txt")
+text = input("Enter your txt: ")
 
-def encrypte_text(secret, text):
-    result = []
-    for i in range(len(text)):
-        text_char = text[i]
-        secret_char = secret[i % len(secret)]
-        char_to_add = ord(text_char) + ord(secret_char)
-        result += [char_to_add]
-    return result
+result = ""
+for i, text_char in enumerate(text):
+    secret_char = secret[i % len(secret)]
+    char_to_add = chr(ord(text_char) + ord(secret_char))
+    result += char_to_add
 
-print(encrypte_text(secret, text))
+print(result)
